3d3a143e4e06f7c699653d05c44ef0f86ae6ba92
chobkyu <qudqud97@naver.com>
2024-07-04 16:46:04 +0900

    update

--- user/app.py
+++ user/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, jsonify, session, request, redirect
 import json
+import mysql.connector
+import os
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/api/user',methods=["POST"])
 def login():
+    connection = mysql.connector.connect(
+        host=os.getenv('MYSQL_HOST', 'db'),
+        user=os.getenv('MYSQL_USER', 'root'),
+        password=os.getenv('MYSQL_PASSWORD', 'password'),
+        database=os.getenv('MYSQL_DATABASE', 'test_db')
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users;")
+    result = cursor.fetchall()
+    
+    print(result)
     session['username'] = request.form["username"]
     return redirect("/w/login", code=302)
 
@@ -17,10 +30,27 @@ def get():
     else:
         return ("Session not found: ")
     
+
 @app.route("/api/user/logout", methods=["GET"])
 def logout():
     session.clear()
     return redirect('/w/login',code=302)
+
+
+@app.route("/api/user/test",methods=["GET"])
+def test():
+    connection = mysql.connector.connect(
+        host=os.getenv('MYSQL_HOST', 'db'),
+        user=os.getenv('MYSQL_USER', 'root'),
+        password=os.getenv('MYSQL_PASSWORD', 'password'),
+        database=os.getenv('MYSQL_DATABASE', 'test_db')
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users;")
+    result = cursor.fetchall()
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(
